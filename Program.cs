9e7fc0d65314a5357de2671c6da86073be06ee68
Baris <bariscakdi@gmail.com>
2024-10-27 11:04:32 +0300

    Proje hazırlandı

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 
+using FeedBackApp.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+
 namespace FeedBackApp
 {
     public class Program
@@ -13,15 +17,15 @@ namespace FeedBackApp
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+            //if (app.Environment.IsDevelopment())
+            //{
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            //}
 
             app.UseHttpsRedirection();
 
