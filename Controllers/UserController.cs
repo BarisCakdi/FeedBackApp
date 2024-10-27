24a4999fe3f65ac5f8c4c3aa3eb55ff704e0f826
Baris <bariscakdi@gmail.com>
2024-10-27 13:10:14 +0300

    Controller ekleme

---
+++ Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using FeedBackApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FeedBackApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UserController(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+
+}
