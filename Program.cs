8c3bf550e627ee6b132aaeb8296eeec21730a821
Baris <bariscakdi@gmail.com>
2024-10-27 17:07:10 +0300

    ok

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 
 using FeedBackApp.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using System;
+using System.Reflection;
 
 namespace FeedBackApp
 {
@@ -16,17 +18,28 @@ namespace FeedBackApp
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Feedback API", Version = "v1" });
+                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+
+                // c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+            });
             builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             //if (app.Environment.IsDevelopment())
             //{
                 app.UseSwagger();
-                app.UseSwaggerUI();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Feedback API V1");
+                c.RoutePrefix = string.Empty;
+            });
             //}
-            builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
 
             app.UseHttpsRedirection();
 
